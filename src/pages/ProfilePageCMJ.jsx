5a26038dd3b798c891d59b1f2ffdd763f5bc4e5a
mjcoding0702 <chungmangjie2000@gmail.com>
2024-04-04 11:06:06 +0800

    change to actual age

--- src/pages/ProfilePageCMJ.jsx
+++ src/pages/ProfilePageCMJ.jsx
@@ -2,7 +2,7 @@ export default function ProfilePageCMJ() {
   return (
     <>
       <p>Hello from the other sideeeeeee 4 apr 2024</p>
-      <p>My ageeee is 23!</p>
+      <p>My ageeee is 24!</p>
     </>
   )
 }
