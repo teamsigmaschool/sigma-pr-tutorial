086515202ef799915bec6a25e3bdb88fe4eb36fe
Leow Kean Tat <leowkeantat@gmail.com>
2024-04-04 10:05:26 +0800

    Created a personal version of the Project's Profile Page (Checkpoint: Slide #47).

---
+++ src/pages/ProfilePageKaz.jsx
@@ -0,0 +1,8 @@
+export default function ProfilePageKaz() {
+    return (
+        <>
+            <p>Hello from Kaz!</p>
+            <p>My physical age is <i>probably</i>62 but my mental age is <b>likely</b> 13.</p>
+        </>
+    )
+}
