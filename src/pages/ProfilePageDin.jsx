bc01be0636335ce76ffc0a74eccfae12290cbed5
Naqiuddinr <mnaqiuddin.rsl@gmail.com>
2024-04-04 11:07:07 +0800

    comment amended

--- src/pages/ProfilePageDin.jsx
+++ src/pages/ProfilePageDin.jsx
@@ -4,7 +4,7 @@ export default function ProfilePageDin() {
     return (
         <>
             <p>Hello from Naqiuddin</p>
-            <p>My name is 19, very young</p>
+            <p>But I am 19</p>
         </>
     )
 }
