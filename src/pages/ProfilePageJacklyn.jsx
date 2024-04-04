a2c17f6fdd342f6867b2ca93c08bda692b70214e
Jacklyn <lamjacklyn@gmail.com>
2024-04-04 11:06:28 +0800

    edit today's age error

--- src/pages/ProfilePageJacklyn.jsx
+++ src/pages/ProfilePageJacklyn.jsx
@@ -2,7 +2,7 @@ export default function ProfilePageJacklyn() {
     return (
         <>
             <p>Hello from Jacklyn!</p>
-            <p>I am 18!</p>
+            <p>I am still 18!</p>
         </>
     )
 }
