5184b90a3afb505d90a2de77f3dca81ce59edee7
Leow Kean Tat <leowkeantat@gmail.com>
2024-04-04 11:06:23 +0800

    Amended "age" typo.

--- src/pages/ProfilePageKaz.jsx
+++ src/pages/ProfilePageKaz.jsx
@@ -2,7 +2,7 @@ export default function ProfilePageKaz() {
     return (
         <>
             <p>Hello from Kaz!</p>
-            <p>My physical ageeee has now advanced to <i>probably</i>93 but my mental ageeee is now <b>mayhaps</b> 4.</p>
+            <p>My physical age has now advanced to <i>probably</i>93 but my mental age is now <b>mayhaps</b> 4.</p>
         </>
     )
 }
