4fdeba555167520e936e86123391555388cbb1c9
roufkhalid93 <roufkhalid93@gmail.com>
2024-07-17 10:58:08 +0800

    add age to profile page

--- src/pages/ProfilePageAR.jsx
+++ src/pages/ProfilePageAR.jsx
@@ -1,8 +1,9 @@
 //change to your own file name
-export default function ProfilePageTest() {
+export default function ProfilePageAR() {
     return (
         <>
-            <p>Hello from Test!</p>
+            <p>Hello from Rouf!</p>
+            <p>My ageee is -3</p>
         </>
     )
 }
