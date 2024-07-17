b536936e356863b974b0a2bea5ec43ae6e7c51a8
Ryan-Hozy <ryanho.rhzy@gmail.com>
2024-07-17 10:57:31 +0800

    Add age to the profile page

--- src/pages/ProfilePageRyan.jsx
+++ src/pages/ProfilePageRyan.jsx
@@ -1,7 +1,10 @@
 
 const ProfilePageRyan = () => {
   return (
-    <div>Hello From Ryan</div>
+    <>
+    <p>Hello From Ryan</p>
+    <p>My ageeeeeeeeeeeeeeeeeeeeeeee is 29!</p>
+    </>
   )
 }
 
