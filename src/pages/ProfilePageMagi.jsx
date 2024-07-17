c7e1f79edcf458b3948cebf529ee6574b350e860
magiananthan <magiananthan2106@gmail.com>
2024-07-17 10:58:20 +0800

    Add age to ProfilePage

--- src/pages/ProfilePageMagi.jsx
+++ src/pages/ProfilePageMagi.jsx
@@ -1,7 +1,8 @@
-export default function ProfilePageTest() {
+export default function ProfilePageMagi() {
     return (
         <>
             <p>Hello from Magi!</p>
+            <p>My ageeee is 26!</p>
         </>
     )
 }
